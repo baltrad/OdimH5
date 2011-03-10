2cad9893f80f14dead4543d02dc826b78c7699a6
Lukasz Wojtas <lukasz.wojtas@imgw.pl>
2011-03-10 09:55:19 +0100

    manual updated

--- src/pl/imgw/odimH5/Main.java
+++ src/pl/imgw/odimH5/Main.java
@@ -1,9 +1,18 @@
 /*
  * OdimH5 :: Converter software for OPERA Data Information Model
- * Remote Sensing Department, Institute of Meteorology and Water Management
- * Maciej Szewczykowski, 2009
+ * Ground Based Remote Sensing Department, Institute of Meteorology and Water Management
  *
+ * OdimH5 is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * OdimH5 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Lesser General Public License along with OdimH5.  If not, see <http://www.gnu.org/licenses/>
+ * 
+ * Maciej Szewczykowski, 2009
  * maciej.szewczykowski@imgw.pl
+ * Łukasz Wojtas 2010
+ * lukasz.wojtas@imgw.pl
+ * 
  */
 
 package pl.imgw.odimH5;
@@ -26,7 +35,7 @@ import pl.imgw.odimH5.util.MessageLogger;
 public class Main {
 
     public static final String VERSION = "2.14";
-    
+
     /**
      * Main method creating
      * 
@@ -36,13 +45,13 @@ public class Main {
     public static void main(String[] args) {
 
         LogsHandler.saveProgramLogs("System starts");
-        
+
         HDF5Model proc = new HDF5Model();
         RainbowModel rainbow = new RainbowModel();
         CommandLineArgsParser cmd = new CommandLineArgsParser();
         MessageLogger msgl = new MessageLogger();
         DataProcessorController cont = new DataProcessorController();
-                
+
         rainbow.setHDFModel(proc);
         rainbow.setMessageLogger(msgl);
         proc.setMessageLogger(msgl);
@@ -51,7 +60,7 @@ public class Main {
         cont.setHDFModel(proc);
         cont.setRAINBOWModel(rainbow);
         cont.startProcessor(args);
-        
+
     }
 
 }
